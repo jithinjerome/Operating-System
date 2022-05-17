a3b98e0be6724cb7cf66193449fcfdf2a72c7151
Jithin Jerome <97607784+jithinjerome@users.noreply.github.com>
2022-05-18 00:05:19 +0530

    Basic Shell Programming.

---
+++ program2.sh
@@ -0,0 +1,18 @@
+#*******************************************************************************
+#CREATOR : JITHIN JEROME
+#TITLE - BASIC SHELL PROGRAM.
+#DATE - MAY , 2022
+#*******************************************************************************
+echo "Enter your name "
+read name
+echo "Enter your semester "
+read sem
+echo "Enter your roll no: "
+read rn
+echo "Enter your register number: "
+read reg_no
+
+echo "Name : $name"
+echo "Semester : $sem"
+echo "Roll no : $rn"
+echo "Register no : $reg_no"
